654761c3cd83a6633d9f29d86bf4eb772794f8de
yh4396777 <70876899+yh4396777@users.noreply.github.com>
2020-09-07 13:43:29 +0800

    Add files via upload

---
+++ new12/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace new12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            string[] b = new string[10];
+
+
+            b[0] = textBox1.Text;
+            b[1] = "+";
+            b[2] = textBox2.Text;
+            int a = Convert.ToInt32(b[0]);
+            int c = Convert.ToInt32(b[2]);
+            int d;
+            switch (b[1])
+            {
+                case "+":
+                    d = a + c;
+                   textBox3.Text=Convert.ToString(d);
+                    break;
+                case "-":
+                    d = a - c;
+                    textBox3.Text = Convert.ToString(d);
+                    break;
+
+                case "*":
+                    d = a * c;
+                    textBox3.Text = Convert.ToString(d);
+                    break;
+                case "/":
+                    d = a / c;
+                    textBox3.Text = Convert.ToString(d);
+                    break;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] b = new string[10];
+
+
+            b[0] = textBox1.Text;
+            b[1] = "-";
+            b[2] = textBox2.Text;
+            int a = Convert.ToInt32(b[0]);
+            int c = Convert.ToInt32(b[2]);
+            int d;
+            switch (b[1])
+            {
+                case "+":
+                    d = a + c;
+                    textBox3.Text = Convert.ToString(d);
+                    break;
+                case "-":
+                    d = a - c;
+                    textBox3.Text = Convert.ToString(d);
+                    break;
+
+                case "*":
+                    d = a * c;
+                    textBox3.Text = Convert.ToString(d);
+                    break;
+                case "/":
+                    d = a / c;
+                    textBox3.Text = Convert.ToString(d);
+                    break;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[] b = new string[10];
+
+
+            b[0] = textBox1.Text;
+            b[1] = "*";
+            b[2] = textBox2.Text;
+            int a = Convert.ToInt32(b[0]);
+            int c = Convert.ToInt32(b[2]);
+            int d;
+            switch (b[1])
+            {
+                case "+":
+                    d = a + c;
+                    textBox3.Text = Convert.ToString(d);
+                    break;
+                case "-":
+                    d = a - c;
+                    textBox3.Text = Convert.ToString(d);
+                    break;
+
+                case "*":
+                    d = a * c;
+                    textBox3.Text = Convert.ToString(d);
+                    break;
+                case "/":
+                    d = a / c;
+                    textBox3.Text = Convert.ToString(d);
+                    break;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string[] b = new string[10];
+
+
+            b[0] = textBox1.Text;
+            b[1] = "/";
+            b[2] = textBox2.Text;
+            int a = Convert.ToInt32(b[0]);
+            int c = Convert.ToInt32(b[2]);
+            int d;
+            switch (b[1])
+            {
+                case "+":
+                    d = a + c;
+                    textBox3.Text = Convert.ToString(d);
+                    break;
+                case "-":
+                    d = a - c;
+                    textBox3.Text = Convert.ToString(d);
+                    break;
+
+                case "*":
+                    d = a * c;
+                    textBox3.Text = Convert.ToString(d);
+                    break;
+                case "/":
+                    d = a / c;
+                    textBox3.Text = Convert.ToString(d);
+                    break;
+            }
+        }
+    }
+}
---
+++ new13/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace new13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = Convert.ToInt32(Console.ReadLine());
+            zhishu(a);
+           
+        }
+        private static void zhishu(int a)
+        {
+            int[] b = new int[100];
+            int c = 0;
+            int g = 0;
+            int h = 0;
+            int[] d = new int[100];
+            for (int i = 2; i < a; i++)
+            {
+                if (a % i == 0)
+                {
+                    b[c] = i;
+                    c++;
+                }
+            }
+
+
+            for (int i = 0; i < c + 1; i++)
+            {
+
+                for (int f = 2; f < b[i]; f++)
+                {
+
+
+                    if (b[i] % f == 0)
+                        h++;
+                }
+
+
+                if (h == 0)
+                {
+                    d[g] = b[i];
+                    g++;
+                }
+
+
+            }
+
+            for (int i = 0; i < g + 1; i++)
+            {
+                if (d[i] != 0)
+                    Console.WriteLine(d[i]);
+
+            }
+        }
+    }
+}
